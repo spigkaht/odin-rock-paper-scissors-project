5c00a20b7bffaa4cb585bc93cd868356500ee3f7
spigkaht <benj@betterrentals.com.au>
2024-02-11 19:01:16 +1100

    Completed re-work of Rock Paper Scissors exercise for Odin JavaScript Exercises
    Added UI and removed console.log statements

--- js/script.js
+++ js/script.js
@@ -19,22 +19,30 @@ function getComputerChoice() {
 // }
 
 function winOrLose(player, computer) {
+  const output = document.querySelector(".output");
+
   if (player === computer) {
-    console.log("Draw this round!")
+    const noWinner = document.createElement("p");
+    noWinner.textContent = "Draw this round!";
+    output.appendChild(noWinner);
     return;
   } else if (
     (computer === 1 && player === 2) ||
     (computer === 2 && player === 3) ||
     (computer === 3 && player === 1)
   ) {
-    console.log("Player won this round!");
+    const playerRoundWinner = document.createElement("p");
+    playerRoundWinner.textContent = "Player won this round!";
+    output.appendChild(playerRoundWinner);
     return -1;
   } else if (
     (computer === 2 && player === 1) ||
     (computer === 3 && player === 2) ||
     (computer === 1 && player === 3)
   ) {
-    console.log("Computer won this round!");
+    const computerRoundWinner = document.createElement("p");
+    computerRoundWinner.textContent = "Computer won this round!";
+    output.appendChild(computerRoundWinner);
     return 1;
   } else console.log("uh oh!");
 }
@@ -53,16 +61,21 @@ function outputString(number) {
 function playRound(player, computer) {
   let string = "";
   string = outputString(player);
-  console.log("Players choice is: ", string);
   string = outputString(computer);
-  console.log("Computers choice is: ", string);
   return winOrLose(player, computer);
 }
 
 function gameWinner(player, computer) {
+  const output = document.querySelector(".output");
   if (player > computer) {
-    console.log("Player is the winner!");
-  } else console.log("Computer is the winner!");
+    const playerWinner = document.createElement("p");
+    playerWinner.textContent = "Player is the winner!";
+    output.appendChild(playerWinner);
+  } else {
+    const computerWinner = document.createElement("p");
+    computerWinner.textContent = "Computer is the winner!";
+    output.appendChild(computerWinner);
+  }
   return;
 }
 
@@ -71,10 +84,26 @@ function clearScreen() {
   clearDiv.innerHTML = "";
 }
 
+function outputToScreen(player, playerChoice, computerChoice) {
+  const playerChoiceOutput = document.createElement("p");
+  playerChoiceOutput.textContent = `Player's choice: ${outputString(player)}`;
+  const computerChoiceOutput = document.createElement("p");
+  computerChoiceOutput.textContent = `Computer's choice: ${outputString(
+    computerChoice
+  )}`;
+  const screenOutput = document.querySelector(".output");
+  screenOutput.appendChild(playerChoiceOutput);
+  screenOutput.appendChild(computerChoiceOutput);
+  const playerScoreOutput = document.createElement("p");
+  playerScoreOutput.textContent = `Player's Score: ${playerScore}`;
+  screenOutput.appendChild(playerScoreOutput);
+  const computerScoreOutput = document.createElement("p");
+  computerScoreOutput.textContent = `Computer's Score: ${computerScore}`;
+  screenOutput.appendChild(computerScoreOutput);
+}
+
 function playGame(player) {
-  while (
-    (playerScore !== 3 && computerScore !== 3)
-  ) {
+  while (playerScore !== 3 && computerScore !== 3) {
     let playerChoice = player;
     let computerChoice = getComputerChoice();
 
@@ -85,25 +114,22 @@ function playGame(player) {
     } else if (roundResult === -1) {
       playerScore++;
     }
-  
-    const playerChoiceOutput = document.createElement("p");
-    playerChoiceOutput.textContent = `Player's choice: ${outputString(player)}`;
-    const computerChoiceOutput = document.createElement("p");
-    computerChoiceOutput.textContent = `Computer's choice: ${outputString(computerChoice)}`;
-    const screenOutput = document.querySelector(".output");
-    screenOutput.appendChild(playerChoiceOutput);
-    screenOutput.appendChild(computerChoiceOutput);
-    const playerScoreOutput = document.createElement("p");
-    playerScoreOutput.textContent = `Player's Score: ${playerScore}`;
-    screenOutput.appendChild(playerScoreOutput);
-    const computerScoreOutput = document.createElement("p");
-    computerScoreOutput.textContent = `Computer's Score: ${computerScore}`;
-    screenOutput.appendChild(computerScoreOutput);
 
-    return;
+    outputToScreen(player, playerChoice, computerChoice);
+
+    if (playerScore !== 3 && computerScore !== 3) {
+      return;
+    }
   }
 
   gameWinner(playerScore, computerScore);
+  
+  const buttons = document.querySelector("#buttons");
+  buttons.innerHTML = "";
+  const refreshPage = document.createElement("button");
+  refreshPage.id = "refresh"
+  refreshPage.textContent = "Replay";
+  buttons.appendChild(refreshPage);
 }
 
 let rockButton = document.querySelector("#rock");
@@ -123,3 +149,8 @@ scissorsButton.addEventListener("click", () => {
   clearScreen();
   playGame(2);
 });
+
+// let refreshButton = document.querySelector("#refresh");
+// refreshButton.addEventListener("click", () => {
+//   location.reload();
+// });
